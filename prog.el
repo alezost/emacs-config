alezost/emacs-config

commit c9b277ab5444a585b6bc3b355e1954a45e4912ba

Fix slime-autodoc key bindings

prog.el
@@ -180,6 +180,15 @@
    ("M-r" . slime-repl-previous-matching-input))
   (al/add-hook-maybe 'slime-repl-mode-hook 'paredit-mode))
 
+(use-package slime-autodoc
+  :defer t
+  :config
+  ;; `slime-autodoc-mode' binds some useless keys into "C-c C-d" prefix.
+  (al/clean-map 'slime-autodoc-mode-map)
+  (bind-keys
+   :map slime-autodoc-mode-map
+   ("SPC" slime-autodoc-space)))
+
 
 ;;; Scheme, geiser
 

commit f349001325e3fae4e62f99ae2821ea78f9beef0e

Fix typo
This typo in a key binding was introduced in commit c9b277a.

prog.el
@@ -187,7 +187,7 @@
   (al/clean-map 'slime-autodoc-mode-map)
   (bind-keys
    :map slime-autodoc-mode-map
-   ("SPC" slime-autodoc-space)))
+   ("SPC" . slime-autodoc-space)))
 
 
 ;;; Scheme, geiser