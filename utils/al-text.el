alezost/emacs-config

commit 376ff003c49a6678168cf0abac177da7965b3fd5

Rename code from "utl-*" to "al/*"

utils/al-text.el
@@ -11,7 +11,7 @@
 ;;; Searching and replacing
 
 ;;;###autoload
-(defun utl-re-search-forward (regexp)
+(defun al/re-search-forward (regexp)
   "The function is similar to `re-search-forward' except it continues
 the search from the beginning of the buffer if it did not succeed."
   (interactive "sRegExp search: ")
@@ -27,18 +27,18 @@ the search from the beginning of the buffer if it did not succeed."
 
 ;;; Editing
 
-(defvar utl-delimiter
+(defvar al/delimiter
   "\f\n"
   "String for separating text in elisp code.")
 
 ;;;###autoload
-(defun utl-insert-delimiter ()
-  "Insert `utl-delimiter' at point."
+(defun al/insert-delimiter ()
+  "Insert `al/delimiter' at point."
   (interactive)
-  (insert utl-delimiter))
+  (insert al/delimiter))
 
 ;;;###autoload
-(defun utl-insert-date (&optional arg)
+(defun al/insert-date (&optional arg)
   "Insert date at point.
 If ARG is nil, use current date.
 If ARG is non-nil, prompt for a date."
@@ -48,7 +48,7 @@ If ARG is non-nil, prompt for a date."
             (format-time-string "%Y-%m-%d"))))
 
 ;;;###autoload
-(defun utl-insert-clipboard ()
+(defun al/insert-clipboard ()
   "Insert the clipboard contents.
 It doesn't destroy what you paste with \\[yank]."
   (interactive)
@@ -59,7 +59,7 @@ It doesn't destroy what you paste with \\[yank]."
         (insert clp)
       (message "Clipboard is empty."))))
 
-(defun utl-yank-or-pop (n)
+(defun al/yank-or-pop (n)
   "Replace just-yanked text with the N-th kill.
 If last command is not `yank', call `yank' N times."
   (if (eq last-command 'yank)
@@ -67,44 +67,44 @@ If last command is not `yank', call `yank' N times."
     (dotimes (i (abs n)) (yank))))
 
 ;;;###autoload
-(defun utl-yank-or-prev (arg)
+(defun al/yank-or-prev (arg)
   "Replace just-yanked text with the previous kill.
-See `utl-yank-or-pop' for details."
+See `al/yank-or-pop' for details."
   (interactive "p")
-  (utl-yank-or-pop arg))
+  (al/yank-or-pop arg))
 
 ;;;###autoload
-(defun utl-yank-or-next (arg)
+(defun al/yank-or-next (arg)
   "Replace just-yanked text with the next kill.
-See `utl-yank-or-pop' for details."
+See `al/yank-or-pop' for details."
   (interactive "p")
-  (utl-yank-or-pop (- arg)))
+  (al/yank-or-pop (- arg)))
 
 ;;;###autoload
-(defun utl-flush-blank-lines (start end)
+(defun al/flush-blank-lines (start end)
   "Delete all empty lines in selected region."
   (interactive "r")
   (flush-lines "^\\s-*$" start end nil))
 
 ;;;###autoload
-(defun utl-delete-blank-lines ()
+(defun al/delete-blank-lines ()
   "Delete blank lines.
-If region is active, call `utl-flush-blank-lines',
+If region is active, call `al/flush-blank-lines',
 otherwise call `delete-blank-lines'."
   (interactive)
   (if (region-active-p)
-      (utl-flush-blank-lines (region-beginning) (region-end))
+      (al/flush-blank-lines (region-beginning) (region-end))
     (delete-blank-lines)))
 
 ;;;###autoload
-(defun utl-kill-line (arg)
+(defun al/kill-line (arg)
   "Similar to `kill-line' but kill including its terminating newline."
   (interactive "p")
   (kill-region (point)
                (progn (forward-visible-line arg) (point))))
 
 ;;;###autoload
-(defun utl-backward-kill-line (arg)
+(defun al/backward-kill-line (arg)
   "Kill line to its beginning.
 With prefix argument ARG, kill that many lines backward including current."
   (interactive "p")
@@ -112,7 +112,7 @@ With prefix argument ARG, kill that many lines backward including current."
                (progn (forward-visible-line (- 1 arg)) (point))))
 
 ;;;###autoload
-(defun utl-save-line (arg)
+(defun al/save-line (arg)
   "Similar to `kill-line' but save in a kill ring without killing."
   (interactive "p")
   (kill-ring-save (point)
@@ -122,8 +122,8 @@ With prefix argument ARG, kill that many lines backward including current."
                     (point))))
 
 ;;;###autoload
-(defun utl-backward-save-line (arg)
-  "Similar to `utl-backward-kill-line' but save in a kill ring without killing."
+(defun al/backward-save-line (arg)
+  "Similar to `al/backward-kill-line' but save in a kill ring without killing."
   (interactive "p")
   (kill-ring-save (point)
                   (save-excursion
@@ -131,7 +131,7 @@ With prefix argument ARG, kill that many lines backward including current."
                     (point))))
 
 ;;;###autoload
-(defun utl-save-whole-line (arg)
+(defun al/save-whole-line (arg)
   "Save current line as if killed, but don't kill it.
 With ARG, save that many lines."
   (interactive "p")
@@ -146,7 +146,7 @@ With ARG, save that many lines."
 ;; Some ideas came from
 ;; <http://stackoverflow.com/questions/88399/how-do-i-duplicate-a-whole-line-in-emacs/>.
 ;;;###autoload
-(defun utl-duplicate-dwim (&optional n)
+(defun al/duplicate-dwim (&optional n)
   "Duplicate current line, or region if it is active.
 Leave the point on the last copy.
 With argument N, make N copies.
@@ -179,7 +179,7 @@ With negative N, comment everything except the last copy."
       (or regionp (move-to-column col)))))
 
 ;;;###autoload
-(defun utl-save-word (arg)
+(defun al/save-word (arg)
   "Save characters forward until encountering the end of a word.
 Save word as if killed, but don't kill it.
 With argument ARG, do this that many times."
@@ -188,15 +188,15 @@ With argument ARG, do this that many times."
                   (save-excursion (forward-word arg) (point))))
 
 ;;;###autoload
-(defun utl-backward-save-word (arg)
+(defun al/backward-save-word (arg)
   "Save characters backward until encountering the end of a word.
 Save word as if killed, but don't kill it.
 With argument ARG, do this that many times."
   (interactive "p")
-  (utl-save-word (- (or arg 1))))
+  (al/save-word (- (or arg 1))))
 
 ;;;###autoload
-(defun utl-save-sexp (arg)
+(defun al/save-sexp (arg)
   "Save characters forward until encountering the end of a sexp.
 Save sexp as if killed, but don't kill it.
 With argument ARG, do this that many times."
@@ -205,15 +205,15 @@ With argument ARG, do this that many times."
                   (save-excursion (forward-sexp arg) (point))))
 
 ;;;###autoload
-(defun utl-backward-save-sexp (arg)
+(defun al/backward-save-sexp (arg)
   "Save characters backward until encountering the end of a sexp.
 Save sexp as if killed, but don't kill it.
 With argument ARG, do this that many times."
   (interactive "p")
-  (utl-save-sexp (- (or arg 1))))
+  (al/save-sexp (- (or arg 1))))
 
 ;;;###autoload
-(defun utl-decode-region (beg end)
+(defun al/decode-region (beg end)
   "Replace selected text hexified by a browser with decoded one."
   (interactive "r")
   (let ((str (org-link-unescape
@@ -223,7 +223,7 @@ With argument ARG, do this that many times."
     (insert str)
     (message "String '%s' was decoded." str)))
 
-(defun utl-get-string (&optional msg)
+(defun al/get-string (&optional msg)
   "Return a string from selected region or prompt for it.
 Use message MSG in a prompt."
   (if (use-region-p)
@@ -231,7 +231,7 @@ Use message MSG in a prompt."
     (read-string (or msg "Enter a string: "))))
 
 ;;;###autoload
-(defun utl-downcase-dwim (arg)
+(defun al/downcase-dwim (arg)
   "Use `downcase-region', if region is active, and `downcase-word' otherwise."
   (interactive "p")
   (if (use-region-p)
@@ -239,7 +239,7 @@ Use message MSG in a prompt."
     (downcase-word arg)))
 
 ;;;###autoload
-(defun utl-upcase-dwim (arg)
+(defun al/upcase-dwim (arg)
   "Use `upcase-region', if region is active, and `upcase-word' otherwise."
   (interactive "p")
   (if (use-region-p)
@@ -247,7 +247,7 @@ Use message MSG in a prompt."
     (upcase-word arg)))
 
 ;;;###autoload
-(defun utl-capitalize-dwim (arg)
+(defun al/capitalize-dwim (arg)
   "Use `capitalize-region', if region is active, and `capitalize-word' otherwise."
   (interactive "p")
   (if (use-region-p)
@@ -255,7 +255,7 @@ Use message MSG in a prompt."
     (capitalize-word arg)))
 
 ;;;###autoload
-(defun utl-delete-horizontal-space (&optional direction)
+(defun al/delete-horizontal-space (&optional direction)
   "Delete all spaces and tabs around point.
 If DIRECTION is positive, delete them after point,
 if it's negative - delete before point."
@@ -279,17 +279,17 @@ if it's negative - delete before point."
     (delete-region beg end)))
 
 ;;;###autoload
-(defun utl-comment-dwirm (arg)
+(defun al/comment-dwirm (arg)
   "Call the comment command you want (Do What I Really Mean).
 Similar to `comment-dwim' except if the region is not active,
 call `comment-line'."
   (interactive "p")
   (if (use-region-p)
       (comment-dwim nil)
-    (utl-comment-line arg)))
+    (al/comment-line arg)))
 
 ;;;###autoload
-(defun utl-comment-line (arg)
+(defun al/comment-line (arg)
   "Comment or uncomment current line.
 If a prefix ARG is non-nil, use that many lines."
   (interactive "p")
@@ -299,7 +299,7 @@ If a prefix ARG is non-nil, use that many lines."
                                (point-at-eol arg)))
 
 ;;;###autoload
-(defun utl-dabbrev-expand-word (arg)
+(defun al/dabbrev-expand-word (arg)
   "Expand current word.
 Like `dabbrev-expand' but use word symbols only."
   (interactive "*P")
@@ -308,16 +308,16 @@ Like `dabbrev-expand' but use word symbols only."
 
 ;;; Change number at point
 
-(defvar utl-number-re "\\<\\([0-9]+\\)\\>"
-  "Regexp for `utl-number-change' functions.
+(defvar al/number-re "\\<\\([0-9]+\\)\\>"
+  "Regexp for `al/number-change' functions.
 First parenthesized expression must match the number.")
 
-(defun utl-number-change (n)
+(defun al/number-change (n)
   "Change the number at the point by N.
 If no number at the point, search forward til the end of the line."
   (save-excursion
-    (or (org-in-regexp utl-number-re)
-        (re-search-forward utl-number-re (line-end-position) t)
+    (or (org-in-regexp al/number-re)
+        (re-search-forward al/number-re (line-end-position) t)
         (error "No number in the current line"))
     (let* ((beg     (match-beginning 1))
            (end     (match-end       1))
@@ -329,18 +329,18 @@ If no number at the point, search forward til the end of the line."
       (message "Number %d was changed to number %d" old-num new-num))))
 
 ;;;###autoload
-(defun utl-number-up (&optional arg)
+(defun al/number-up (&optional arg)
   "Increase the number at the point by one.
 With prefix ARG, change that many numbers."
   (interactive "p")
-  (utl-number-change arg))
+  (al/number-change arg))
 
 ;;;###autoload
-(defun utl-number-down (&optional arg)
+(defun al/number-down (&optional arg)
   "Decrease the number at the point by one.
 With prefix ARG, change that many numbers."
   (interactive "p")
-  (utl-number-change (- arg)))
+  (al/number-change (- arg)))
 
 
 ;;; Changing the case of previous word(s)
@@ -348,55 +348,55 @@ With prefix ARG, change that many numbers."
 ;; Idea from <http://www.emacswiki.org/emacs/sequential-command.el>.
 
 ;; Example of key bindings:
-;;   (global-set-key (kbd "s-d") 'utl-downcase-word-backward)
-;;   (global-set-key (kbd "s-c") 'utl-capitalize-word-backward)
-;;   (global-set-key (kbd "s-u") 'utl-upcase-word-backward)
+;;   (global-set-key (kbd "s-d") 'al/downcase-word-backward)
+;;   (global-set-key (kbd "s-c") 'al/capitalize-word-backward)
+;;   (global-set-key (kbd "s-u") 'al/upcase-word-backward)
 
 ;; When a key binding is pressed, the previous word is changed, if it
 ;; (or another key bound to those function) is pressed again, the word
 ;; before the previous is changed and so on.
 
-(defvar utl-word-position nil
+(defvar al/word-position nil
   "Last saved position.
-Used for `utl-downcase-word-backward',
-`utl-capitalize-word-backward' and `utl-upcase-word-backward'.")
+Used for `al/downcase-word-backward',
+`al/capitalize-word-backward' and `al/upcase-word-backward'.")
 
-(defvar utl-word-seq-functions nil
+(defvar al/word-seq-functions nil
   "List of commands for sequential modifying the case of a word.")
 
-(defmacro utl-change-word-backward (name fun)
+(defmacro al/change-word-backward (name fun)
   "Make a function for sequential changing previous word(s).
-Resulting function `utl-NAME-word-backward' will be added to
-`utl-word-seq-functions'.
+Resulting function `al/NAME-word-backward' will be added to
+`al/word-seq-functions'.
 Function FUN is called in body of the resulting function for updating
 the word.  It should accept a number of modified words as argument."
-  (let ((fun-name (intern (concat "utl-" name "-word-backward"))))
-    (add-to-list 'utl-word-seq-functions fun-name)
+  (let ((fun-name (intern (concat "al/" name "-word-backward"))))
+    (add-to-list 'al/word-seq-functions fun-name)
     `(defun ,fun-name (arg)
        ,(concat (capitalize name)
                 " previous word (or ARG words), do not move the point.\n"
                 "Multiple calls will change previous words sequentially.")
        (interactive "p")
        (save-excursion
-         (when (memq last-command utl-word-seq-functions)
-           (goto-char utl-word-position))
+         (when (memq last-command al/word-seq-functions)
+           (goto-char al/word-position))
          (backward-word arg)
-         (setq utl-word-position (point))
+         (setq al/word-position (point))
          (,fun arg)))))
 
-(utl-change-word-backward "downcase" downcase-word)
-(utl-change-word-backward "capitalize" capitalize-word)
-(utl-change-word-backward "upcase" upcase-word)
+(al/change-word-backward "downcase" downcase-word)
+(al/change-word-backward "capitalize" capitalize-word)
+(al/change-word-backward "upcase" upcase-word)
 
-;;;###autoload (autoload 'utl-downcase-word-backward "utl-text" nil t)
-;;;###autoload (autoload 'utl-capitalize-word-backward "utl-text" nil t)
-;;;###autoload (autoload 'utl-upcase-word-backward "utl-text" nil t)
+;;;###autoload (autoload 'al/downcase-word-backward "al/text" nil t)
+;;;###autoload (autoload 'al/capitalize-word-backward "al/text" nil t)
+;;;###autoload (autoload 'al/upcase-word-backward "al/text" nil t)
 
 
 ;;; Moving
 
 ;;;###autoload
-(defun utl-beginning-of-line ()
+(defun al/beginning-of-line ()
   "Move point to beginning of current line.
 If the point is in the beginning of line already,
 move to beginning of previous one."
@@ -404,7 +404,7 @@ move to beginning of previous one."
   (beginning-of-line (if (= (point) (point-at-bol)) 0 1)))
 
 ;;;###autoload
-(defun utl-end-of-line ()
+(defun al/end-of-line ()
   "Move point to end of current line.
 If the point is in the end of line already,
 move to end of next one."
@@ -412,13 +412,13 @@ move to end of next one."
   (end-of-line (if (= (point) (point-at-eol)) 2 1)))
 
 ;;;###autoload
-(defun utl-recenter-top ()
+(defun al/recenter-top ()
   "Move current line to the top (+1) of the window."
   (interactive)
   (recenter-top-bottom 1))
 
 ;;;###autoload
-(defun utl-recenter-end-of-buffer-top ()
+(defun al/recenter-end-of-buffer-top ()
   "Move the last line (-1) of the buffer to the top of the window."
   (interactive)
   (goto-char (point-max))
@@ -428,14 +428,14 @@ move to end of next one."
 
 ;;; Misc
 
-(defvar utl-check-parens-modes
+(defvar al/check-parens-modes
   '(emacs-lisp-mode lisp-mode scheme-mode)
-  "List of modes where `utl-check-parens' is called.")
+  "List of modes where `al/check-parens' is called.")
 
 ;;;###autoload
-(defun utl-check-parens ()
-  "Run `check-parens' if current mode is one of `utl-check-parens-modes'."
-  (when (memq major-mode utl-check-parens-modes)
+(defun al/check-parens ()
+  "Run `check-parens' if current mode is one of `al/check-parens-modes'."
+  (when (memq major-mode al/check-parens-modes)
     (check-parens)))
 
 (provide 'al-text)

init/dict.el
@@ -22,21 +22,21 @@
  :prefix-map al/translation-map
  :prefix-docstring "Map for dictionaries, translating and friends."
  :prefix "<XF86Spell>"
- ("<XF86Spell>" . utl-dictem-run-word)
+ ("<XF86Spell>" . al/dictem-run-word)
  ("s" . dictem-run-search)
  ("m" . dictem-run-match)
- ("i" . utl-dictem-run-show-all-info)
- ("d" . utl-dictem-run-dict-search)
+ ("i" . al/dictem-run-show-all-info)
+ ("d" . al/dictem-run-dict-search)
  ("q" . dictem-kill-all-buffers)
- ("e"   (utl-google-translate-using-languages "en" "ru"))
- ("r"   (utl-google-translate-using-languages "ru" "en"))
- ("f"   (utl-google-translate-using-languages "fr" "ru"))
+ ("e"   (al/google-translate-using-languages "en" "ru"))
+ ("r"   (al/google-translate-using-languages "ru" "en"))
+ ("f"   (al/google-translate-using-languages "fr" "ru"))
  ("l"   (let ((google-translate-translation-directions-alist
                '(("la" . "ru") ("ru" . "la")
                  ("la" . "en") ("en" . "la"))))
-          (utl-google-translate-smooth-translate)))
+          (al/google-translate-smooth-translate)))
  ("g"   (let ((google-translate-translation-directions-alist nil))
-          (utl-google-translate-smooth-translate))))
+          (al/google-translate-smooth-translate))))
 
 
 ;;; Misc settings and packages
@@ -58,9 +58,9 @@
 (with-eval-after-load 'dictem
   (when (require 'al-dictem nil t)
     (dictem-initialize)
-    (advice-add 'dictem :override 'utl-dictem)
+    (advice-add 'dictem :override 'al/dictem)
     (advice-add 'dictem-define-on-press
-      :override 'utl-dictem-define-on-press))
+      :override 'al/dictem-define-on-press))
   (setq dictem-use-existing-buffer nil)
   (al/bind-keys
    :map dictem-mode-map
@@ -82,7 +82,7 @@
             'dictem-postprocess-definition-hyperlinks))
 
 (with-eval-after-load 'al-dictem
-  (setq utl-dictem-dicts
+  (setq al/dictem-dicts
         '(nil "mueller7" "korolew_en-ru" "korolew_ru-en"
               "slovnyk_ru-en" "ushakov" "fd-eng-lat" "fd-lat-eng")))
 

init/file-modes.el
@@ -56,7 +56,7 @@
    org-ellipsis " […]"
    org-file-apps
    '(("\\.mm\\'" . default)
-     ("\\.x?html?\\'" utl-choose-browser file)
+     ("\\.x?html?\\'" al/choose-browser file)
      ("\\.png\\'" . "sxiv %s")
      ("\\.pdf\\'" . "zathura %s")
      ("\\.djvu\\'" . "zathura %s")
@@ -88,9 +88,9 @@
       ("C-M-H-u" . org-forward-element)
       ("C-M-H-." . org-up-element)
       ("C-M-H-e" . org-down-element)
-      ("<C-kp-enter>" . utl-org-table-next-column)
-      ("<M-kp-enter>" . utl-org-table-kill-rows-recalculate)
-      ("<C-M-kp-enter>" . utl-org-table-next-table)
+      ("<C-kp-enter>" . al/org-table-next-column)
+      ("<M-kp-enter>" . al/org-table-kill-rows-recalculate)
+      ("<C-M-kp-enter>" . al/org-table-next-table)
       ("C-c e" . org-export-dispatch)
       ("C-c M-x" . org-copy-special)
       ("C-c C-t" . org-cut-special)
@@ -100,7 +100,7 @@
     '(al/org-keys al/text-editing-keys))
 
   (when (require 'al-ido nil t)
-    (advice-add 'org-set-tags :around #'utl-ido-disable))
+    (advice-add 'org-set-tags :around #'al/ido-disable))
 
   (org-add-link-type "pdfview" 'org-pdfview-open 'org-pdfview-export))
 
@@ -123,9 +123,9 @@
    ("e" . org-agenda-next-line)))
 
 (with-eval-after-load 'al-org
-  (org-add-link-type "emms" 'utl-org-emms-open)
+  (org-add-link-type "emms" 'al/org-emms-open)
   (al/add-hook-maybe 'org-store-link-functions
-    'utl-org-emms-store-link))
+    'al/org-emms-store-link))
 
 (al/autoload "org-pdfview"
   org-pdfview-open

init/files.el
@@ -25,22 +25,22 @@
  :prefix-docstring "Map for finding files."
  :prefix "C-f"
  ("C-f"   . ido-find-file)
- ("S"     . utl-sudo-find-file)
- ("h"     . utl-ssh-find-file)
- ("z"     . utl-router-get-log)
+ ("S"     . al/sudo-find-file)
+ ("h"     . al/ssh-find-file)
+ ("z"     . al/router-get-log)
  ("u"     . browse-url-emacs)
  ("l"     . find-library)
- ("q"       (utl-ido-find-file
+ ("q"       (al/ido-find-file
              (expand-file-name "package-build/recipes/"
                                quelpa-build-dir)))
- ("e"       (utl-ido-find-file al/emacs-dir))
- ("C-c"     (utl-ido-find-file al/emacs-init-dir))
+ ("e"       (al/ido-find-file al/emacs-dir))
+ ("C-c"     (al/ido-find-file al/emacs-init-dir))
  ("C-s"     (find-file (al/emacs-init-dir-file "settings.el")))
  ("k"       (find-file (al/emacs-init-dir-file "keys.el")))
  ("i"       (find-file (al/emacs-init-dir-file "init.el")))
  ("t"       (find-file (al/emacs-init-dir-file "text.el")))
  ("v"       (find-file (al/emacs-init-dir-file "visual.el")))
- ("c"       (utl-ido-find-file (al/emacs-my-packages-dir-file "alect-themes")))
+ ("c"       (al/ido-find-file (al/emacs-my-packages-dir-file "alect-themes")))
  ("C-M-c"   (find-file (al/emacs-my-packages-dir-file
                         "alect-themes/alect-themes.el"))))
 
@@ -52,23 +52,23 @@
  ("n"     . bookmark-set)
  ("k"     . bookmark-delete)
  ("l"     . bookmark-bmenu-list)
- ("S"     . utl-sr-toggle)
- ("h"       (utl-ido-find-file "~"))
- ("d"       (utl-ido-find-file al/journal-dir))
- ("w"       (utl-ido-find-file al/download-dir))
+ ("S"     . al/sr-toggle)
+ ("h"       (al/ido-find-file "~"))
+ ("d"       (al/ido-find-file al/journal-dir))
+ ("w"       (al/ido-find-file al/download-dir))
  ("e"       (find-file al/echo-download-dir))
- ("M-n"     (utl-ido-find-file al/notes-dir))
- ("t"       (utl-ido-find-file al/tmp-dir))
- ("m"       (utl-ido-find-file al/music-dir))
- ("p"       (utl-ido-find-file al/progs-dir))
- ("b"       (utl-ido-find-file (al/progs-dir-file "bash")))
- ("g"       (utl-ido-find-file (al/progs-dir-file "guile")))
- ("M-c"     (utl-ido-find-file al/config-dir))
+ ("M-n"     (al/ido-find-file al/notes-dir))
+ ("t"       (al/ido-find-file al/tmp-dir))
+ ("m"       (al/ido-find-file al/music-dir))
+ ("p"       (al/ido-find-file al/progs-dir))
+ ("b"       (al/ido-find-file (al/progs-dir-file "bash")))
+ ("g"       (al/ido-find-file (al/progs-dir-file "guile")))
+ ("M-c"     (al/ido-find-file al/config-dir))
  ("C-M-c"   (find-file (al/config-dir-file "config.scm")))
- ("M-g"     (utl-ido-find-file al/guix-profile-dir))
- ("c"       (utl-ido-find-file (al/config-dir-file "conkeror")))
- ("s"       (utl-ido-find-file (al/config-dir-file "stumpwm")))
- ("v"       (utl-ido-find-file "/var/log")))
+ ("M-g"     (al/ido-find-file al/guix-profile-dir))
+ ("c"       (al/ido-find-file (al/config-dir-file "conkeror")))
+ ("s"       (al/ido-find-file (al/config-dir-file "stumpwm")))
+ ("v"       (al/ido-find-file "/var/log")))
 
 (al/bind-keys
  :prefix-map al/grep-find-map
@@ -98,9 +98,9 @@
  vc-make-backup-files t)
 
 (when (require 'al-file nil t)
-  (setq backup-enable-predicate 'utl-backup-enable-predicate)
+  (setq backup-enable-predicate 'al/backup-enable-predicate)
   (advice-add 'make-backup-file-name-1
-    :override 'utl-make-backup-file-name-1))
+    :override 'al/make-backup-file-name-1))
 
 
 ;;; Dired
@@ -115,13 +115,13 @@
    dired-recursive-deletes 'always)
 
   (defconst al/dired-keys
-    '(("SPC"   . utl-dired-get-size)
+    '(("SPC"   . al/dired-get-size)
       ("N"     . dired-create-directory)
-      ("M"     . utl-dired-man-or-chmod)
+      ("M"     . al/dired-man-or-chmod)
       ("f"     . dired-show-file-type)
-      ("F"     . utl-dired-stat)
+      ("F"     . al/dired-stat)
       ("o"     . dired-up-directory)
-      ("u"     . utl-dired-find-file)
+      ("u"     . al/dired-find-file)
       ("U"     . dired-do-find-marked-files)
       ("."     . dired-previous-line)
       ("e"     . dired-next-line)
@@ -129,8 +129,8 @@
       ("E"     . dired-next-dirline)
       ("C-M-." . dired-prev-dirline)
       ("C-M-e" . dired-next-dirline)
-      ("H-a"   . utl-dired-beginning-of-buffer)
-      ("H-i"   . utl-dired-end-of-buffer)
+      ("H-a"   . al/dired-beginning-of-buffer)
+      ("H-i"   . al/dired-end-of-buffer)
       ("M-d"   . dired-toggle-read-only)
       ("p"     . pathify-dired)
       ("t"     . image-dired-display-thumbs)
@@ -141,7 +141,7 @@
       ("d"     . dired-display-file)
       ("C-d"   . dired-find-file-other-window)
       ("C-l"   . dired-omit-mode)
-      ("c 0"   . utl-default-directory-to-kill-ring)
+      ("c 0"   . al/default-directory-to-kill-ring)
       ("c RET"   (dired-copy-filename-as-kill 0))
       ("r"     . dired-do-query-replace-regexp)
       ("C-ь p" . emms-play-dired)
@@ -164,14 +164,14 @@
    :prefix-map al/dired-open-file-map
    :prefix-docstring "Map for opening files in external programs in dired."
    :prefix "C-j"
-   ("M-j"   (utl-dired-start-process "xdg-open"))
-   ("C-j" . utl-dired-open-file)
-   ("v d"   (utl-dired-start-process "baobab"))
-   ("v f"   (utl-dired-start-process "gdmap" "-f"))
-   ("m"     (utl-dired-start-process "mupdf"))
-   ("z"     (utl-dired-start-process "zathura"))
-   ("s"     (utl-dired-start-process-on-marked-files "sxiv"))
-   ("c"     (utl-browse-url-conkeror
+   ("M-j"   (al/dired-start-process "xdg-open"))
+   ("C-j" . al/dired-open-file)
+   ("v d"   (al/dired-start-process "baobab"))
+   ("v f"   (al/dired-start-process "gdmap" "-f"))
+   ("m"     (al/dired-start-process "mupdf"))
+   ("z"     (al/dired-start-process "zathura"))
+   ("s"     (al/dired-start-process-on-marked-files "sxiv"))
+   ("c"     (al/browse-url-conkeror
              (browse-url-file-url (dired-get-filename))))
    ("w"     (w3m-browse-url
              (browse-url-file-url (dired-get-filename)))))
@@ -179,12 +179,12 @@
   (al/add-hook-maybe 'dired-mode-hook 'hl-line-mode)
 
   (when (require 'al-mode-line nil t)
-    (utl-mode-line-default-buffer-identification 'dired-mode))
+    (al/mode-line-default-buffer-identification 'dired-mode))
 
   (require 'dired-x nil t)
   (when (require 'al-dired nil t)
     (advice-add 'dired-sort-set-mode-line
-      :override 'utl-dired-sort-set-mode-line)))
+      :override 'al/dired-sort-set-mode-line)))
 
 (setq
  dired-guess-shell-gnutar "tar"
@@ -211,7 +211,7 @@
 (with-eval-after-load 'dired-aux
   (when (require 'al-dired nil t)
     (advice-add 'dired-mark-read-file-name
-      :override 'utl-dired-mark-read-file-name)))
+      :override 'al/dired-mark-read-file-name)))
 
 (with-eval-after-load 'wdired
   (al/bind-keys-from-vars 'wdired-mode-map)
@@ -231,7 +231,7 @@
    ("o"     . image-dired-display-previous-thumbnail-original)
    ("u"     . image-dired-display-next-thumbnail-original)
    ("C-M-m" . image-dired-unmark-thumb-original-file)
-   ("DEL"   . utl-image-dired-unmark-thumb-original-file-backward)))
+   ("DEL"   . al/image-dired-unmark-thumb-original-file-backward)))
 
 
 ;;; Misc settings and packages
@@ -276,8 +276,8 @@
 
 (with-eval-after-load 'al-file
   (setq
-   utl-ssh-default-user (list user-login-name "root" "lena")
-   utl-ssh-default-host "hyperion"))
+   al/ssh-default-user (list user-login-name "root" "lena")
+   al/ssh-default-host "hyperion"))
 
 (with-eval-after-load 'sunrise-commander
   (setq

init/keys.el
@@ -316,7 +316,7 @@ VARS are variables with bindings supported by
  ("C-="           (describe-char (point)))
  ("C-c r"       . revert-buffer)
  ("C-c p"       . list-processes)
- ("C-c k"       . utl-kill-process))
+ ("C-c k"       . al/kill-process))
 
 (defalias 'ctl-x-r-prefix ctl-x-r-map)
 (al/bind-key "M-R" ctl-x-r-prefix)
@@ -340,7 +340,7 @@ VARS are variables with bindings supported by
  :prefix-map al/modes-map
  :prefix-docstring "Map for enabling/disabling modes."
  :prefix "M-M"
- ("M-M" . utl-major-mode-to-kill-ring)
+ ("M-M" . al/major-mode-to-kill-ring)
  ("a" . artist-mode)
  ("A" . auto-fill-mode)
  ("c" . conf-unix-mode)

commit 1e5a76bcda7b0a324cc81999297fe7cb91948011

utils: text: Fix typos in autoload clauses
This regression was introduced by commit
376ff003c49a6678168cf0abac177da7965b3fd5.

* utils/al-text.el: Rename "al/text" to "al-text" in autoloads.

utils/al-text.el
@@ -388,9 +388,9 @@ the word.  It should accept a number of modified words as argument."
 (al/change-word-backward "capitalize" capitalize-word)
 (al/change-word-backward "upcase" upcase-word)
 
-;;;###autoload (autoload 'al/downcase-word-backward "al/text" nil t)
-;;;###autoload (autoload 'al/capitalize-word-backward "al/text" nil t)
-;;;###autoload (autoload 'al/upcase-word-backward "al/text" nil t)
+;;;###autoload (autoload 'al/downcase-word-backward "al-text" nil t)
+;;;###autoload (autoload 'al/capitalize-word-backward "al-text" nil t)
+;;;###autoload (autoload 'al/upcase-word-backward "al-text" nil t)
 
 
 ;;; Moving