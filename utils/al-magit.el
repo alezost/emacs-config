1f932fa950f3890cb8a842b82420a0b7fe836c2c
Alex Kost <alezost@gmail.com>
2016-06-26 20:42:06 +0300

    utils: magit: Remove 'al/magit-popup-substitute-key'
    * utils/al-magit.el (al/magit-popup-substitute-key): Remove as I don't
      use it since commit b2047e25da29b8d698a7adcd58413443d32d21f7.

--- utils/al-magit.el
+++ utils/al-magit.el
@@ -17,30 +17,7 @@
 
 ;;; Code:
 
-(require 'cl-lib)
 (require 'ido)
-
-(defun al/magit-popup-substitute-key (var-name property old-char new-char)
-  "Change key binding of action/switch/option of a magit popup VAR-NAME.
-
-PROPERTY should be one of the following magit keywords:
-`:actions', `:options', `:keys'.
-
-For example, if you want to use 'g' instead of 'm' for 'grep'
-option in a log popup, use:
-
-  (al/magit-popup-substitute-key 'magit-log-popup :options ?m ?g)"
-  (let* ((var-val    (symbol-value var-name))
-         (searched-p (lambda (elem) (eq (car elem) old-char)))
-         (old-keys   (plist-get var-val property))
-         (old-key    (cl-find-if searched-p old-keys)))
-    (if old-key
-        (let* ((new-key (cl-substitute new-char old-char old-key
-                                       :test #'eq))
-               (new-keys (cl-substitute-if new-key searched-p old-keys)))
-          (set var-name (plist-put var-val property new-keys)))
-      (message "Key '%c' does not exist in '%S' %S."
-               old-char var-name property))))
 
 ;;;###autoload
 (defun al/magit-ido-switch-buffer ()
