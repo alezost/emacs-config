9bc85302e80b15ce8007242429bfe000d6e8a818
Alex Kost <alezost@gmail.com>
2016-10-23 13:18:32 +0300

    utils: Add 'al/emms-mpv-tq-enqueue-sync'
    * utils/al-emms-mpv.el (al/emms-mpv-tq-enqueue-sync): New procedure.

--- utils/al-emms-mpv.el
+++ utils/al-emms-mpv.el
@@ -1,4 +1,4 @@
-;;; al-emms-mpv.el --- Additional functionality for using EMMS with mpv
+;;; al-emms-mpv.el --- Additional functionality for using EMMS with mpv  -*- lexical-binding: t -*-
 
 ;; Copyright © 2015-2016 Alex Kost
 
@@ -6,12 +6,12 @@
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -28,6 +28,21 @@ COMMAND is what may be put in mpv conf-file, e.g.: 'cycle mute',
     (tq-enqueue emms-player-simple-mpv--tq
                 (concat command "\n")   ; newline is vital
                 "" nil #'ignore)))
+
+(defun al/emms-mpv-tq-enqueue-sync (com-ls closure fn
+                                           &optional delay-question)
+  "Like `emms-player-simple-mpv-tq-enqueue' but synchronized.
+I.e., wait for the result of FN and return it."
+  (let (done result)
+    (emms-player-simple-mpv-tq-enqueue
+     com-ls closure
+     (lambda (clos ans)
+       (setq result (funcall fn clos ans)
+             done t))
+     delay-question)
+    (while (not done)
+      (sleep-for 0.1))
+    result))
 
 (defun al/emms-mpv-show-progress ()
   "Show progress in the OSD of the current video."
