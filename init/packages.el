241e9aa01b336a87ba2ba1b655f43cba4e7cc46b
Alex Kost <alezost@gmail.com>
2017-01-08 23:31:15 +0300

    init/guix: Fallback to a default value for 'guix-directory'
    This should have been done in commit
    784af2a9685921ed420b1ac26cb05c93e111b071.

--- init/packages.el
+++ init/packages.el
@@ -1,17 +1,17 @@
 ;;; packages.el --- Emacs packages and interfaces to other package systems
 
-;; Copyright © 2014-2016 Alex Kost
+;; Copyright © 2014-2017 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -126,9 +126,10 @@
 
 (setq
  guix-current-profile al/guix-user-profile-dir
- guix-directory (al/first-existing-file
-                 (al/src-dir-file "guix")
-                 (al/devel-dir-file "guix")))
+ guix-directory (or (al/first-existing-file
+                     (al/src-dir-file "guix")
+                     (al/devel-dir-file "guix"))
+                    guix-directory))
 
 (al/bind-keys
  :prefix-map al/guix-map
