a19b9eb64718fc21a27f0a2afd7ceabb3c83ee06
Alex Kost <alezost@gmail.com>
2017-01-12 23:54:40 +0300

    init: Use "C-t g" for games

--- init/games.el
+++ init/games.el
@@ -1,17 +1,17 @@
 ;;; games.el --- Various settings for games
 
-;; Copyright © 2014-2016 Alex Kost
+;; Copyright © 2014–2017 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -20,10 +20,11 @@
 
 ;;; Global keys
 
-(al/bind-keys*
+(al/bind-keys
+ :map ctl-x-map
  :prefix-map al/games-map
  :prefix-docstring "Map for games."
- :prefix "M-G"
+ :prefix "g"
  ("p"   (practice-words 1000 1))
  ("T" . tetris)
  ("t" . typing-game)
