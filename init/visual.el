30fc749d1b0c29aea82e82d7f5e04bf29fb45dfc
Alex Kost <alezost@gmail.com>
2017-01-28 21:39:15 +0300

    init/dim: Change mode name of 'xref--xref-buffer-mode'

--- init/visual.el
+++ init/visual.el
@@ -1,17 +1,17 @@
 ;;; visual.el --- Visual settings: fonts, themes, mode-line, …  -*- lexical-binding: t -*-
 
-;; Copyright © 2012-2016 Alex Kost
+;; Copyright © 2012–2017 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -165,6 +165,7 @@
      (snippet-mode               "🗍")
      (diary-mode                 "🕮")
      (ediff-mode                 "ε")
+     (xref--xref-buffer-mode     "xref")
 
      (gnus-server-mode           "𝗚Srv")
      (gnus-browse-mode           "𝗚Srv🗋")
