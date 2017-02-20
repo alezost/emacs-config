alezost/emacs-config

commit 87170c79263e8ba5a4df6b17a7fa21c52b75b614

init: Load custom file in the end of init file
Instead of loading it in the 'after-init-hook'.

init/init.el
@@ -177,11 +177,8 @@
           "net"
           "dict"
           "visual"
-          "games")))
-
-;; (setq custom-file "/tmp/custom.el")
-(setq custom-file (al/emacs-init-dir-file "custom.el"))
-(al/eval-after-init (load custom-file 'noerror))
+          "games"
+          "custom")))
 
 (message "Garbage collected %d times." gcs-done)
 

commit 851468f93d5b497292901e996ab084bef854041e

init: Set custom-file
This fixes a regression introduced by commit
87170c79263e8ba5a4df6b17a7fa21c52b75b614.

init/init.el
@@ -1,6 +1,6 @@
 ;;; init.el --- Init file  -*- lexical-binding: t -*-
 
-;; Copyright © 2012-2016 Alex Kost
+;; Copyright © 2012–2017 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -179,6 +179,8 @@
           "visual"
           "games"
           "custom")))
+
+(setq custom-file (al/emacs-init-dir-file "custom.el"))
 
 (message "Garbage collected %d times." gcs-done)
 