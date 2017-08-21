7a11ca406e46910c67597f635167ab3ed600d9e9
Alex Kost <alezost@gmail.com>
2017-08-21 21:36:53 +0300

    utils/sql: Require 'al-mode-line'
    * utils/al-sql.el (al/sql-highlight-product): Require 'al-mode-line'
    instead of "defvar"-ing 'al/mode-info', because if 'al-sql' was loaded
    before 'al-mode-line', it would lead to a global setting of
    'al/mode-info' (thus to the wrong mode-line).

--- utils/al-sql.el
+++ utils/al-sql.el
@@ -1,17 +1,17 @@
 ;;; al-sql.el --- Additional functionality for sql stuff
 
-;; Copyright © 2013-2016 Alex Kost
+;; Copyright © 2013–2017 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -80,7 +80,7 @@ Use `al/sql-history-dir'."
 
 ;;; Mode line
 
-(defvar al/mode-info)
+(require 'al-mode-line)
 
 (defun al/sql-highlight-product ()
   "Add sql product name to `al/mode-info' instead of `mode-name'.
