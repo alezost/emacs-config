08862422dbb5a7522c3a344a57f3fb09820cbd7a
Alex Kost <alezost@gmail.com>
2018-07-02 22:22:52 +0300

    utils/org: Use iterating for table recalculation
    * utils/al-org.el (al/org-table-kill-rows-recalculate): Call
      'org-table-recalculate' with 'iterate' argument.

--- utils/al-org.el
+++ utils/al-org.el
@@ -1,17 +1,17 @@
 ;;; al-org.el --- Additional functionality for org-mode
 
-;; Copyright © 2012-2016 Alex Kost
+;; Copyright © 2012–2016, 2018 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -80,7 +80,8 @@ row."
 	(if (equal "" (org-table-get (org-table-current-line) cur-col))
 	    (org-table-kill-row)
 	  (forward-line))))
-    (org-table-recalculate t)))
+    (org-table-recalculate 'iterate 'no-align)
+    (org-table-align)))
 
 (defun al/org-table-next-table ()
   "Move point to the next org-table in the current buffer"
