caf9f164ebda0ec85db114972cfc26de7ef90882
Alex Kost <alezost@gmail.com>
2018-07-02 22:21:25 +0300

    utils/erc: Avoid compilation warnings
    * utils/al-erc.el: Add missing "require"-s to silence byte-compiler.

--- utils/al-erc.el
+++ utils/al-erc.el
@@ -1,17 +1,17 @@
 ;;; al-erc.el --- Additional functionality for ERC
 
-;; Copyright © 2013-2016 Alex Kost
+;; Copyright © 2013–2016, 2018 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -19,6 +19,9 @@
 
 (require 'erc)
 (require 'erc-log)
+(require 'erc-networks)
+(require 'erc-stamp)
+(require 'erc-track)
 
 ;;;###autoload
 (defun al/erc-number-of-users ()
