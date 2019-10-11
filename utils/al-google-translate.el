15132ad5da180e91552046c0d7c71201ad29535d
Alex Kost <alezost@gmail.com>
2019-10-11 16:28:51 +0300

    utils/google-translate: Remove the code for "default UI"
    As I have not used it for several years (I use "smooth UI" instead).

--- utils/al-google-translate.el
+++ utils/al-google-translate.el
@@ -18,54 +18,7 @@
 ;;; Code:
 
 (require 'cl-lib)
-(require 'google-translate-default-ui)
 (require 'google-translate-smooth-ui)
-(require 'al-read)
-
-
-;;; Default UI
-
-(defun al/%google-translate (override-p reverse-p)
-  "Translate region or prompting text.
-Alternative to `%google-translate-query-translate' and
-`%google-translate-at-point'."
-  (let* ((langs (google-translate-read-args override-p reverse-p))
-         (source-language (car langs))
-         (target-language (cadr langs)))
-    (google-translate-translate
-     source-language target-language
-     (if (use-region-p)
-         (buffer-substring-no-properties (region-beginning) (region-end))
-       (al/read-string
-        (format "Translate from %s to %s: "
-                (google-translate-language-display-name source-language)
-                (google-translate-language-display-name target-language))
-        nil nil (current-word t t))))))
-
-;;;###autoload
-(defun al/google-translate (&optional override-p)
-  "Translate region or prompting text from auto detected language.
-For the meaning of OVERRIDE-P, see `google-translate-query-translate'."
-  (interactive "P")
-  (let ((google-translate-default-source-language "auto"))
-    (al/%google-translate override-p nil)))
-
-;;;###autoload
-(defun al/google-translate-direct ()
-  "Translate region or prompting text from source to target language.
-For details look at `google-translate-query-translate'."
-  (interactive)
-  (al/%google-translate nil nil))
-
-;;;###autoload
-(defun al/google-translate-reverse ()
-  "Translate region or prompting text from target to source language.
-For details look at `google-translate-query-translate'."
-  (interactive)
-  (al/%google-translate nil t))
-
-
-;;; Smooth UI
 
 ;;;###autoload
 (defun al/google-translate-smooth-translate ()
