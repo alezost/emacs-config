eeccf5167c426b0d8a1b47bf49a71e25f7014f62
Alex Kost <alezost@gmail.com>
2020-01-01 10:53:45 +0300

    utils: Make argument for 'al/set-isearch-input-method' optional

--- utils/al-misc-cmd.el
+++ utils/al-misc-cmd.el
@@ -1,17 +1,17 @@
 ;;; al-misc-cmd.el --- Miscellaneous interactive commands
 
-;; Copyright © 2013-2016 Alex Kost
+;; Copyright © 2013–2016, 2019 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -84,7 +84,7 @@ With prefix, prompt for directory as well."
 ;;; Spelling and languages
 
 ;;;###autoload
-(defun al/set-isearch-input-method (input-method)
+(defun al/set-isearch-input-method (&optional input-method)
   "Activate input method INPUT-METHOD in interactive search.
 See `set-input-method' for details."
   (set-input-method input-method)
