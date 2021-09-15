d562eb432002ebff9d59b94711eee9a1b038d06d
Alex Kost <alezost@gmail.com>
2021-09-15 18:04:29 +0300

    utils/elisp: Deactivate mark after evaluating a region

--- utils/al-elisp.el
+++ utils/al-elisp.el
@@ -1,17 +1,17 @@
 ;;; al-elisp.el --- Additional functionality for elisp, eldoc
 
-;; Copyright © 2013-2016 Alex Kost
+;; Copyright © 2013–2016, 2021 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -33,7 +33,9 @@
 ARG is passed to `eval-last-sexp'."
   (interactive "P")
   (if (use-region-p)
-      (eval-region (region-beginning) (region-end))
+      (progn
+        (eval-region (region-beginning) (region-end))
+        (deactivate-mark))
     (eval-last-sexp arg)))
 
 ;;;###autoload
@@ -42,7 +44,9 @@ ARG is passed to `eval-last-sexp'."
 ARG is passed to `pp-eval-last-sexp'."
   (interactive "P")
   (if (use-region-p)
-      (eval-region (region-beginning) (region-end))
+      (progn
+        (eval-region (region-beginning) (region-end))
+        (deactivate-mark))
     (pp-eval-last-sexp arg)))
 
 ;;;###autoload
