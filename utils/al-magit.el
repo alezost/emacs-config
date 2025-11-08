b70b99dac63fff53a8b8b3311df96edec2a4325c
Alex Kost <alezost@gmail.com>
2025-11-08 13:17:59 +0300

    utils/magit: Improve 'al/magit-switch-buffer'
    * utils/al-magit.el (al/magit-buffers): New function.
      (al/magit-switch-buffer): Use it.  Add optional argument to choose
      from all magit buffers.

--- utils/al-magit.el
+++ utils/al-magit.el
@@ -1,6 +1,6 @@
-;;; al-magit.el --- Additional functionality for magit
+;;; al-magit.el --- Additional functionality for magit  -*- lexical-binding: t -*-
 
-;; Copyright © 2015–2017 Alex Kost
+;; Copyright © 2015–2025 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -21,12 +21,26 @@
 (require 'git-commit)
 (require 'al-buffer)
 
+(defun al/magit-buffers (&optional type)
+  "Return a list of TYPE magit buffers.
+TYPE can be one of the following symbols:
+  `all' (default) - all magit buffers,
+  `status' - only status buffers."
+  (let ((mode (if (eq type 'status)
+                  'magit-status-mode
+                'magit-mode)))
+    (seq-filter (lambda (buf)
+                  (with-current-buffer buf
+                    (derived-mode-p mode)))
+                (buffer-list))))
+
 ;;;###autoload
-(defun al/magit-switch-buffer ()
-  "Switch to a magit status buffer preferably using `ivy-read'."
-  (interactive)
+(defun al/magit-switch-buffer (&optional all)
+  "Prompt for a magit status buffer and switch to it.
+If ALL is non-nil, select from all magit buffers, not only statuses."
+  (interactive "P")
   (al/switch-buffer "Magit buffer: "
-                    :initial-input "*magit: "))
+                    :buffers (al/magit-buffers (if all 'all 'status))))
 
 ;;;###autoload
 (defun al/git-commit-co-authored (name mail)
