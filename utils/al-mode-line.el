6e33aaddf407d5639fcab94065220d94408af549
Alex Kost <alezost@gmail.com>
2025-12-01 10:19:34 +0300

    utils/mode-line: Remove unused leftovers

--- utils/al-mode-line.el
+++ utils/al-mode-line.el
@@ -1,17 +1,17 @@
 ;;; al-mode-line.el --- Additional functionality for mode-line  -*- lexical-binding: t -*-
 
-;; Copyright © 2014-2016 Alex Kost
+;; Copyright © 2014-2025 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -20,26 +20,6 @@
 
 ;;; Mode names
 
-;; Idea from
-;; <http://www.masteringemacs.org/articles/2012/09/10/hiding-replacing-modeline-strings/>.
-
-(defvar al/mode-names-alist nil
-  "Alist of mode names.
-Car of each assoc is a `major-mode'.  Cdr is a string or a
-function returning a string used for `mode-name'.")
-
-;;;###autoload
-(defun al/mode-name (&rest _)
-  "Replace `mode-name' of the current major mode.
-Use the appropriate name from `al/mode-names-alist'.
-This function is intended to be used in `after-change-major-mode-hook'."
-  (interactive)
-  (let ((name (cdr (assq major-mode al/mode-names-alist))))
-    (when name
-      (setq mode-name
-            (if (functionp name) (funcall name) name)))))
-
-;;;###autoload
 (defun al/mode-line-default-buffer-identification (mode)
   "Set `mode-line-buffer-identification' to the default value for MODE.
 Some major modes like to override `mode-line-buffer-identification'.
@@ -53,20 +33,6 @@ like this:
               (lambda ()
                 (setq mode-line-buffer-identification
                       (default-value 'mode-line-buffer-identification))))))
-
-
-;;; Mode line process
-
-(defvar al/mode-line-process '("[%s]")
-  "String used in `al/mode-line-process' function.")
-
-;;;###autoload
-(defun al/mode-line-process ()
-  "Set `mode-line-process' to the value of `al/mode-line-process'.
-This function is intended to be used in hooks:
-
-  (add-hook \\='comint-mode-hook #\\='al/mode-line-process)"
-  (setq mode-line-process al/mode-line-process))
 
 
 ;;; Additional info for major modes
