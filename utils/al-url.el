9f82c2442591072d2fe27eae0953d20af975d72c
Alex Kost <alezost@gmail.com>
2025-12-20 13:23:14 +0300

    utils/url: Improve code
    * utils/al-url.el (al/url-wget-mp3): Require 'wget' here, not at the
      top-level.
      (al/url-mp3-re): Rename to...
      (al/url-mp3-regexp): ... this.

--- utils/al-url.el
+++ utils/al-url.el
@@ -1,27 +1,27 @@
 ;;; al-url.el --- Code for searching and downloading various stuff  -*- lexical-binding: t -*-
 
-;; Copyright © 2015-2016 Alex Kost
+;; Copyright © 2015–2025 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Code:
 
-(require 'wget)
-
-(defvar al/url-mp3-re
+(defvar al/url-mp3-regexp
   (rx "http" (? ?s) "://" (1+ any) ".mp3")
   "Regexp for mp3 file.")
+
+(declare-function wget "wget")
 
 ;;;###autoload
 (defun al/url-wget-mp3 (url)
@@ -29,9 +29,10 @@
   (interactive
    (list (read-string "Download mp3 from URL: "
                       (thing-at-point 'url))))
+  (require 'wget)
   (let* ((buf (url-retrieve-synchronously url))
          (mp3 (with-current-buffer buf
-                (re-search-forward al/url-mp3-re)
+                (re-search-forward al/url-mp3-regexp)
                 (match-string 0))))
     (when (y-or-n-p (format "Download '%s'? " mp3))
       (wget mp3))))
