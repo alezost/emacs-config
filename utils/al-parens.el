9bf1b8e40fb7ca261d7cdc7f88775a0996e9cf47
Alex Kost <alezost@gmail.com>
2026-01-07 18:50:02 +0300

    utils/parens: Autoload smartparens commands
    Instead of requiring 'smartparens'.

--- utils/al-parens.el
+++ utils/al-parens.el
@@ -1,23 +1,24 @@
 ;;; al-parens.el --- Additional functionality for working with parentheses  -*- lexical-binding: t -*-
 
-;; Copyright © 2015-2016 Alex Kost
+;; Copyright © 2015–2026 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Code:
 
-(require 'smartparens)
+(declare-function sp-kill-sexp "smartparens")
+(declare-function sp-backward-kill-sexp "smartparens")
 
 ;;;###autoload
 (defun al/kill-sexp (&optional arg)
