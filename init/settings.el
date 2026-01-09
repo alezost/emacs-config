3f2e14c28594acd25071bfd2db19cc036575a53d
Alex Kost <alezost@gmail.com>
2026-01-09 23:24:35 +0300

    init/settings: Simplify code by using 'seq-keep'

--- init/settings.el
+++ init/settings.el
@@ -447,11 +447,10 @@ This variable is used to set `al/dired-ignored-extensions'.")
   (setq Man-notify-method 'pushy)
   (when (require 'al-file nil t)
     (setq Man-header-file-path
-          (append (delq nil
-                        (mapcar (lambda (p)
-                                  (al/file-if-exists
-                                   (expand-file-name "include" p)))
-                                (al/guix-profiles)))
+          (append (seq-keep (lambda (p)
+                              (al/file-if-exists
+                               (expand-file-name "include" p)))
+                            (al/guix-profiles))
                   Man-header-file-path)))
   (when (require 'al-mode-line nil t)
     (al/mode-line-default-buffer-identification 'Man-mode))
