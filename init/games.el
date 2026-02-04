ca2ed8b274a6346130a64120d69a81fa8aac1906
Alex Kost <alezost@gmail.com>
2026-02-04 18:20:00 +0300

    init/games: Use `al/with-eval-after-load'
    Also remove unused packages ("practice-words", "mana"), get rid of
    compilation warnings and update settings in general.

--- init/games.el
+++ init/games.el
@@ -1,6 +1,6 @@
 ;;; games.el --- Various settings for games  -*- lexical-binding: t -*-
 
-;; Copyright © 2014–2017 Alex Kost
+;; Copyright © 2014–2026 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -23,79 +23,47 @@
  :prefix-map al/games-map
  :prefix-docstring "Map for games."
  :prefix "g"
- ("p"   (practice-words 1000 1))
  ("T" . tetris)
- ("t" . typing-game)
  ("d" . ducpel)
- ("D"   (princ ducpel-moves-history (current-buffer)))
  ("s" . snake))
 
 
 ;;; Misc settings and packages
 
-(with-eval-after-load 'gamegrid
+(al/with-eval-after-load gamegrid
   (setq gamegrid-user-score-file-directory
         (al/emacs-data-dir-file "games")))
 
-(al/autoload "typing-practice" practice-words)
-(with-eval-after-load 'typing-practice
-  (setq typing-practice-time-threshold 2))
-
-(with-eval-after-load 'ducpel
-  (setq
-   ducpel-levels-directory
-   (expand-file-name "ducpel/levels" quelpa-build-dir)
-   ducpel-replay-pause 0.3)
+(al/with-eval-after-load ducpel
+  (setq ducpel-replay-pause 0.3)
   (let ((ducpel-dir (al/emacs-my-packages-dir-file "ducpel")))
     (setq
      ducpel-user-levels-directory
      (file-name-as-directory (expand-file-name "levels" ducpel-dir))
      ducpel-user-saves-directory
      (file-name-as-directory (expand-file-name "temp" ducpel-dir))))
+
   (al/bind-keys
-   :map ducpel-mode-map
-   ("o" . ducpel-move-left)
-   ("u" . ducpel-move-right)
-   ("." . ducpel-move-up)
-   ("e" . ducpel-move-down)
-   ("h" . ducpel-previous-man)
-   ("n" . ducpel-next-man)
-   ("," . ducpel-previous-level)
-   ("p" . ducpel-next-level)
-   ("H-u" . ducpel-undo)))
+    :map al/games-map
+    ("D" (princ ducpel-moves-history (current-buffer))))
 
-(setq
- tetris-width 16
- tetris-height 32
- tetris-buffer-width 40
- tetris-buffer-height 40)
+  (al/bind-keys
+    :map ducpel-mode-map
+    ("o" . ducpel-move-left)
+    ("u" . ducpel-move-right)
+    ("." . ducpel-move-up)
+    ("e" . ducpel-move-down)
+    ("h" . ducpel-previous-man)
+    ("n" . ducpel-next-man)
+    ("," . ducpel-previous-level)
+    ("p" . ducpel-next-level)))
 
-(with-eval-after-load 'tetris
+(al/with-eval-after-load tetris
   (setq
-   tetris-blank-options
-   '(((glyph
-       ((:type xpm :ascent center :data "! XPM2
-16 16 2 1
-+ c #151515
-. c #000000
-++++++++++++++++
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-+..............+
-++++++++++++++++")))
-      (t ?\s))
-     nil nil))
+   tetris-width 10
+   tetris-height 26
+   tetris-buffer-width 30
+   tetris-buffer-height 28)
 
   (al/bind-keys
    :map tetris-mode-map
@@ -107,7 +75,7 @@
    ("RET" . tetris-move-bottom)
    ("SPC" . tetris-pause-game)))
 
-(with-eval-after-load 'snake
+(al/with-eval-after-load snake
   (al/bind-keys
    :map snake-mode-map
    ("o"   . snake-move-left)
@@ -115,8 +83,5 @@
    ("."   . snake-move-up)
    ("e"   . snake-move-down)
    ("SPC" . snake-pause-game)))
-
-(with-eval-after-load 'mana
-  (setq mana-character (rot13 "wbuanguna")))
 
 ;;; games.el ends here
