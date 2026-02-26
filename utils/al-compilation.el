02487d3cbf13b8e2c27ad42be2d077dbed116505
Alex Kost <alezost@gmail.com>
2026-02-26 07:30:28 +0300

    utils/al-compilation: Cosmetic changes

--- utils/al-compilation.el
+++ utils/al-compilation.el
@@ -1,17 +1,17 @@
 ;;; al-compilation.el --- Additional functionality for compilation buffers  -*- lexical-binding: t -*-
 
-;; Copyright © 2015-2016 Alex Kost
+;; Copyright © 2015–2016 Alex Kost
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -38,7 +38,7 @@
 This function is intended to be used in
 `compilation-finish-functions'."
   (with-current-buffer buffer
-    (unless (eq major-mode 'grep-mode)
+    (unless (derived-mode-p 'grep-mode)
       (cl-multiple-value-bind (sound urgency)
           ;; `compilation-start' calls `compilation-handle-exit' with
           ;; "finished" message if exit status is 0.  Is there a better
@@ -46,7 +46,7 @@ This function is intended to be used in
           (if (string-match "\\`finished" reason)
               (list al/compilation-sound-success 'normal)
             (list al/compilation-sound-error 'critical))
-        (and sound (al/play-sound sound))
+        (when sound (al/play-sound sound))
         (notifications-notify
          :urgency urgency
          :title "Compilation finished"
