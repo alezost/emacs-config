e219c4185be831d36ec4fec4478c9b04702f41e4
Alex Kost <alezost@gmail.com>
2026-04-23 07:54:03 +0300

    init/company: Use my default key bindings
    In particular, to make "M-p" work.

--- init/settings.el
+++ init/settings.el
@@ -145,12 +145,13 @@
   (setq
    company-idle-delay nil
    company-show-quick-access t)
-  (al/bind-keys
-    :map company-active-map
-    ("C-." . company-select-previous)
-    ("C-e" . company-select-next)
-    ("M-." . company-select-previous)
-    ("M-e" . company-select-next))
+  (defconst al/company-active-map
+    '(("C-." . company-select-previous)
+      ("C-e" . company-select-next)
+      ("M-." . company-select-previous)
+      ("M-e" . company-select-next))
+    "Alist of auxiliary keys for `company-active-map'.")
+  (al/bind-keys-from-vars 'company-active-map 'al/company-active-map)
   (global-company-mode))
 
 (al/define-multi-command al/tab
