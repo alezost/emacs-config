8fc029a2dcde99bdc9cf78b25f4b3c2a6eda5056
Alex Kost <alezost@gmail.com>
2026-05-27 16:55:58 +0300

    init: Add "Code" comment header everywhere

--- init/init.el
+++ init/init.el
@@ -15,6 +15,8 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Code:
+
 (load (expand-file-name "../utils/general/al-places"
                         (file-name-directory
                          (file-truename load-file-name))))
