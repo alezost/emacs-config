5ef705875bf97effa404a6b932a643f0121408a1
Alex Kost <alezost@gmail.com>
2026-08-15 12:36:38 +0300

    Move aurel settings from "init/packages" to "settings"

--- init/packages.el
+++ init/packages.el
@@ -87,28 +87,6 @@
  ("I"     . aurel-installed-packages))
 
 (al/eval-after-load aurel
-  (setq
-   aurel-download-directory (al/src-dir-file "abs")
-   aurel-aur-user-name "alezost"
-   ;; aurel-aur-user-package-info-check t
-   aurel-info-aur-user-string "——————————————————————————————————————\n"
-   aurel-info-installed-package-string aurel-info-aur-user-string
-   aurel-debug-level 9
-   aurel-list-format
-   '((name aurel-list-get-name 20 t)
-     (version nil 9 t)
-     (installed-version nil 9 t)
-     (maintainer aurel-list-get-maintainer 13 t)
-     (votes nil 8 bui-list-sort-numerically-4 :right-align t)
-     (description nil 30 nil))
-   aurel-list-titles
-   '((votes . "V.")
-     (version . "Ver.")
-     (installed-version . "Inst.")))
-  (defconst al/aurel-filter-keys
-    ;; Default `aurel-enable-filter' was left for backward compatibility.
-    '(("f" . bui-enable-filter))
-    "Alist of auxiliary keys for `aurel-filter-map'.")
-  (al/bind-keys-from-vars 'aurel-filter-map 'al/aurel-filter-keys))
+  (al/load-settings "aurel"))
 
 ;;; packages.el ends here
---
+++ settings/aurel.el
@@ -0,0 +1,31 @@
+;;; aurel.el --- Settings for `aurel' package  -*- lexical-binding: t -*-
+
+(require 'aurel)
+(require 'al-key)
+
+(setq
+ aurel-download-directory (al/src-dir-file "abs")
+ aurel-aur-user-name "alezost"
+ ;; aurel-aur-user-package-info-check t
+ aurel-info-aur-user-string "——————————————————————————————————————\n"
+ aurel-info-installed-package-string aurel-info-aur-user-string
+ aurel-debug-level 9
+ aurel-list-format
+ '((name aurel-list-get-name 20 t)
+   (version nil 9 t)
+   (installed-version nil 9 t)
+   (maintainer aurel-list-get-maintainer 13 t)
+   (votes nil 8 bui-list-sort-numerically-4 :right-align t)
+   (description nil 30 nil))
+ aurel-list-titles
+ '((votes . "V.")
+   (version . "Ver.")
+   (installed-version . "Inst.")))
+
+(defconst al/aurel-filter-keys
+  ;; Default `aurel-enable-filter' was left for backward compatibility.
+  '(("f" . bui-enable-filter)))
+
+(al/bind-keys-from-vars 'aurel-filter-map 'al/aurel-filter-keys)
+
+;;; aurel.el ends here
